d61593bfa247cc3e45445eb7738d989cc227fd45
vigneshmb <vigneshmb2016@gmail.com>
2025-02-19 23:28:52 +0530

    adding Routes - In Progress

---
+++ reactjs/src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import App from "../App";
+import HomePage from "#Pages/HomePage.jsx";
+import BoardListPage from "#Pages/BoardListPage.jsx";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="board" element={<BoardListPage />}>
+        <Route index element={<BoardListPage />} />
+        {/* <Route path="board/:id" element={<Project />} /> */}
+      </Route>
+    </Routes>
+  );
+}
